acd6688f31948ab905dcbb121bb9dbad480ba201
Artem Zhdanov <artem.zhdanov@kohls.com>
2019-07-01 10:33:07 +0300

    Initial commit

--- src/main/java/com/temas/webproxy/Main.java
+++ src/main/java/com/temas/webproxy/Main.java
@@ -2,8 +2,6 @@ package com.temas.webproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Created by azhdanov on 28.06.2019.
@@ -14,13 +12,4 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-//    @Bean
-//    public JettyEmbeddedServletContainerFactory jettyEmbeddedServletContainerFactory() {
-//        JettyEmbeddedServletContainerFactory jettyContainer =
-//                new JettyEmbeddedServletContainerFactory();
-//
-//
-//        jettyContainer.setPort(9099);
-//        return jettyContainer;
-//    }
 }
